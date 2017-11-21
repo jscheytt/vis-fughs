57f9771dc78c0446246d75ffe6346c569cb8243e
Janis92 <janis.uttenweiler@gmail.com>
2017-11-21 15:59:09 +0100

    Signed-off-by: Janis92 <janis.uttenweiler@gmail.com>

--- js/BarChart.js
+++ js/BarChart.js
@@ -1,27 +1,27 @@
-// set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 20},
-    width = 700 - margin.left - margin.right,
-    height = 120 - margin.top - margin.bottom;
+// set the dimensions and margin1s of the graph
+var margin1 = {top: 20, right: 20, bottom: 30, left: 20},
+    width1 = 700 - margin1.left - margin1.right,
+    height1 = 120 - margin1.top - margin1.bottom;
 
 // set the ranges
-var x = d3.scaleBand()
-          .range([0, width])
+var x1 = d3.scaleBand()
+          .range([0, width1])
           .padding(0.1);
-var y = d3.scaleLinear()
-          .range([height, 0]);
+var y1 = d3.scaleLinear()
+          .range([height1, 0]);
           
 // append the svg object to the body of the page
 // append a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("#Timeline").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+// moves the 'group' element to the top left margin1
+var svg1 = d3.select("#barchart").append("svg")
+    .attr("width", width1 + margin1.left + margin1.right)
+    .attr("height", height1 + margin1.top + margin1.bottom)
   .append("g")
     .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
+          "translate(" + margin1.left + "," + margin1.top + ")");
 
 // get the data
-d3.csv("data/BarChart_m.csv", function(error, data) {
+d3.csv("data/BarChart_w.csv", function(error, data) {
   if (error) throw error;
 
   // format the data
@@ -30,26 +30,26 @@ d3.csv("data/BarChart_m.csv", function(error, data) {
   });
 
   // Scale the range of the data in the domains
-  x.domain(data.map(function(d) { return d.time; }));
-  y.domain([0, d3.max(data, function(d) { return d.passenger; })]);
+  x1.domain(data.map(function(d) { return d.time; }));
+  y1.domain([0, d3.max(data, function(d) { return d.passenger; })]);
 
   // append the rectangles for the bar chart
-  svg.selectAll(".bar")
+  svg1.selectAll(".bar")
       .data(data)
     .enter().append("rect")
       .attr("class", "bar")
-      .attr("x", function(d) { return x(d.time); })
-      .attr("width", x.bandwidth())
-      .attr("y", function(d) { return y(d.passenger); })
-      .attr("height", function(d) { return height - y(d.passenger); });
+      .attr("x", function(d) { return x1(d.time); })
+      .attr("width", x1.bandwidth())
+      .attr("y", function(d) { return y1(d.passenger); })
+      .attr("height", function(d) { return height1 - y1(d.passenger); });
 
   // add the x Axis
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+  svg1.append("g")
+      .attr("transform", "translate(0," + height1 + ")")
+      .call(d3.axisBottom(x1));
 
   // add the y Axis
-  svg.append("g")
-      .call(d3.axisLeft(y));
+  svg1.append("g")
+      .call(d3.axisLeft(y1));
 
 });
