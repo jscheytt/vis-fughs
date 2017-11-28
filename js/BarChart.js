852e396e583e1249ff4df6bb5f5f40167c396752
Janis92 <janis.uttenweiler@gmail.com>
2017-11-28 20:45:34 +0100

    Signed-off-by: Janis92 <janis.uttenweiler@gmail.com>

--- js/BarChart.js
+++ js/BarChart.js
@@ -1,16 +1,22 @@
 // set the dimensions and margin1s of the graph
-var margin1 = {top: 20, right: 20, bottom: 30, left: 20},
+var margin1 = {top: 20, right: 60, bottom: 40, left: 20},
     width1 = 700 - margin1.left - margin1.right,
-    height1 = 120 - margin1.top - margin1.bottom;
+    height1 = 130 - margin1.top - margin1.bottom;
 
-// set the ranges
+// declarate Tooltip
+var tooltipJanis = d3.select("div").append("div").attr("class", "toolTip");
+
 var x1 = d3.scaleBand()
           .range([0, width1])
-          .padding(0.1);
+          .padding(0.5);
+		 
 var y1 = d3.scaleLinear()
           .range([height1, 0]);
-          
-// append the svg object to the body of the page
+
+// declarate colours for bars          
+var colours = d3.scaleOrdinal()
+    .range(["#000", "#000"]);
+// append the svg object to the #barchart of the page
 // append a 'group' element to 'svg'
 // moves the 'group' element to the top left margin1
 var svg1 = d3.select("#barchart").append("svg")
@@ -18,38 +24,84 @@ var svg1 = d3.select("#barchart").append("svg")
     .attr("height", height1 + margin1.top + margin1.bottom)
   .append("g")
     .attr("transform", 
-          "translate(" + margin1.left + "," + margin1.top + ")");
+          "translate(" + margin1.left*2 + "," + margin1.top + ")");
 
-// get the data
-d3.csv("data/BarChart_w.csv", function(error, data) {
-  if (error) throw error;
+	// get the data
+	d3.csv("data/BarChart_d.csv", function(error, data) {
+	if (error) throw error;
 
-  // format the data
-  data.forEach(function(d) {
-    d.passenger = +d.passenger;
-  });
+		// format the data
+		data.forEach(function(d) {
+			d.passenger = +d.passenger;
+		});
 
-  // Scale the range of the data in the domains
-  x1.domain(data.map(function(d) { return d.time; }));
-  y1.domain([0, d3.max(data, function(d) { return d.passenger; })]);
+		// Scale the range of the data in the domains
+		x1.domain(data.map(function(d) { return d.timeJanis; }));
+		y1.domain([0, d3.max(data, function(d) { return d.passenger; })]);
 
-  // append the rectangles for the bar chart
-  svg1.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x1(d.time); })
-      .attr("width", x1.bandwidth())
-      .attr("y", function(d) { return y1(d.passenger); })
-      .attr("height", function(d) { return height1 - y1(d.passenger); });
+		// declare y axis with ticks
+		var yAxis1 = d3.axisLeft(y1);
+		yAxis1.ticks(5);
+		
+		// axis label for y axis
+		svg1.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 0 - 40)
+			.attr("x",0 - (height1 / 1.65))
+			.attr("dy", "1em")
+			.style("text-anchor", "middle")
+			.style ("font-size", "10px")
+			.style ("font-weight", "bold")
+			.text("Ein/Aussteiger");
 
-  // add the x Axis
-  svg1.append("g")
-      .attr("transform", "translate(0," + height1 + ")")
-      .call(d3.axisBottom(x1));
+	  
+		// add the x Axis
+		svg1.append("g")
+			.attr("class", "axis axis--x")
+			.attr("transform", "translate(0," + height1 + ")")
+			.call(d3.axisBottom(x1))
+			// rotate elements on x axis 
+			.selectAll("text")
+			.attr("y", 0)
+			.attr("x", 15)
+			.attr("dx", "-0.18em")
+			.attr("dy", ".35em")
+			.attr("transform", "rotate(50)")
+			.style("text-anchor", "start");
+	
+		// x axis label
+		svg1.append("text")
+			.attr("class", "axis axis--y")
+			.attr("transform", "translate(" + (width1*1.01) + " ," + (height1*1) + ")")
+			.style("text-anchor", "right")
+			.style ("font-size", "10px")
+			.style ("font-weight", "bold")
+			.text("Tage");
+	  
+		// only every 5th tick shall be shown
+		var ticksJanis = d3.selectAll(".tick text");
+		ticksJanis.style("display", function (d, i) { return i % 5 ? "none" : "initial" })
 
-  // add the y Axis
-  svg1.append("g")
-      .call(d3.axisLeft(y1));
+		// 
+		svg1.append("g")
+			.call(yAxis1);
+			
+		svg1.selectAll(".bar")
+			.data(data)
+			.enter().append("rect")
+			.attr("class", "bar")
+			.attr("x", function(d) { return x1(d.timeJanis); })
+			.attr("width", x1.bandwidth())
+			.attr("y", function(d) { return y1(d.passenger); })
+			.attr("height", function(d) { return height1 - y1(d.passenger); })
 
-});
+			.attr("fill", function(d) { return colours(d.timeJanis); })
+			.on("mousemove", function(d){
+				tooltipJanis
+				.style("left", d3.event.pageX - 50 + "px")
+				.style("top", d3.event.pageY - 70 + "px")
+				.style("display", "inline-block")
+				.html((d.timeJanis) + "<br>" + "Ein/Aussteiger: " + (d.passenger));
+			})
+			.on("mouseout", function(d){ tooltipJanis.style("display", "none");});
+	});
